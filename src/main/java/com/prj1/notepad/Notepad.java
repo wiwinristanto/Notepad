7b45fede2ed713faf5728fe3db54987eb403d182
Wiwin Ristanto <>
2022-08-31 14:17:35 +0700

    First Commit - fitur simpan dengan klik button Simpan - fitur clear jTextArea dengan klik button Hapus - Simpan file dengan tekan tombol Enter di keyboard "keyPressed"

---
+++ src/main/java/com/prj1/notepad/Notepad.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.prj1.notepad;
+import javax.swing.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
+import java.io.IOException;
+import java.nio.file.StandardOpenOption;
+
+/**
+ *
+ * @author wiwin.ristanto
+ */
+public class Notepad{
+
+    public static void main(String[] args) {
+        
+        
+       
+        Tampilan tp = new Tampilan();
+        tp.setVisible(true);
+        
+    }
+    
+}
